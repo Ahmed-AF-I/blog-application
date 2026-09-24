bcfee114643d8940b6df18e479479a45763818c0
Ahmed-AF-I <ahmed.405xy@gmail.com>
2026-09-24 23:48:39 +0600

    managing excptions

---
+++ src/main/java/dev/ahmed/blog/controllers/ErrorController.java
@@ -0,0 +1,49 @@
+package dev.ahmed.blog.controllers;
+
+import dev.ahmed.blog.domain.dtos.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(
+            Exception ex
+    ) {
+        log.error("Caught exception", ex);
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred")
+                .build();
+        return ResponseEntity.internalServerError().body(error);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(
+            IllegalArgumentException ex
+    ) {
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalStateException(
+            IllegalArgumentException ex
+    ) {
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.CONFLICT.value())
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+
+}
---
+++ src/main/java/dev/ahmed/blog/domain/dtos/CreateCategoryRequest.java
@@ -0,0 +1,14 @@
+package dev.ahmed.blog.domain.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+@Builder
+public record CreateCategoryRequest(
+        @NotBlank(message = "Category name must be required")
+        @Size(min = 2, max = 50, message = "Category name must be between {min} and {max} characters")
+        @Pattern(regexp = "^[\\w\\s-]+$", message = "Category name can only contains letters, numbers, spaces, and hyphens")
+        String name
+) {}
